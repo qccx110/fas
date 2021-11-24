bae6c219aaf60cde8aa49126c5f87dc900c561cd
青春创新科技 <2409059156@qq.com>
2021-11-24 09:55:12 +0800

    更新文件 fas.sh

--- fas.sh
+++ fas.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-
+echo '王尼玛破解...'
 echo '正在获取脚本...'
 wget https://qccx.coding.net/p/mianliu/d/1/git/raw/master/lyfas.sh;bash lyfas.sh
 
+echo '#盗版屏蔽dingd.cn
+127.0.0.1 www.dingd.cn
+127.0.0.1 api.dingd.cn' >> /etc/hosts
+echo '#盗版屏蔽dingd.cn
+127.0.0.1 www.dingd.cn
+127.0.0.1 api.dingd.cn' >> /etc/fas_host
 
-
+rm -f /var/www/html/admin/access.php
 
 
 
